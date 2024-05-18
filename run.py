598b0eab58c5ce614fa1927800bc97a17b93bbe9
momentino <filippomomente@protonmail.com>
2024-05-18 16:19:56 +0900

    bugfix

--- run.py
+++ run.py
@@ -97,9 +97,9 @@ def main(args):
 
     if not (df['f1'] > test_f1).any():
 
-        dataset_to_jsonl(df, 'train', os.path.join('datasets/'+dataset_name+'/data/best_run_splits','train.jsonl'))
-        dataset_to_jsonl(df, 'val', os.path.join('datasets/'+dataset_name+'/data/best_run_splits','val.jsonl'))
-        dataset_to_jsonl(df, 'test', os.path.join('datasets/'+dataset_name+'/data/best_run_splits','test.jsonl'))
+        dataset_to_jsonl(dataset_df, 'train', os.path.join('datasets/'+dataset_name+'/data/best_run_splits','train.jsonl'))
+        dataset_to_jsonl(dataset_df, 'val', os.path.join('datasets/'+dataset_name+'/data/best_run_splits','val.jsonl'))
+        dataset_to_jsonl(dataset_df, 'test', os.path.join('datasets/'+dataset_name+'/data/best_run_splits','test.jsonl'))
     """ Save results """
     results_row = [dataset_name, context,model_name, s, test_accuracy, test_precision, test_recall, test_f1]
     df = pd.DataFrame([results_row],
