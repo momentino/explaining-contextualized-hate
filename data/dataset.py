9e098efe130733aa52435a400cb2fd3c335ba952
momentino <filippomomente@protonmail.com>
2024-05-27 17:48:24 +0900

    bugfix

--- data/dataset.py
+++ data/dataset.py
@@ -14,22 +14,24 @@ class ToxicLangDataset(Dataset):
 
         self.context = context
 
+
+        if(context):
+            if(dataset_name=="pavlopoulos20"):
+                self.df = self.df[self.df['context'].notna()] #take only those where context is not NaN
+        else:
+            if (dataset_name == "pavlopoulos20"):
+                self.df = self.df[self.df['context'].isna()]  # take only those where context is NaN
+
         train_limit = int(0.8 * len(self.df))
         val_limit = int(0.9 * len(self.df))
         # used to split the dataset
         split_separators = {
-            'train': (1,train_limit),
+            'train': (1, train_limit),
             'val': (train_limit, val_limit),
             'test': (val_limit, len(self.df)),
             'no_split': (1, len(self.df))
         }
-        if(context):
-            if(dataset_name=="pavlopoulos20"):
-                self.df = self.df[self.df['context'].notna()] #take only those where context is not NaN
-            self.contexts = self.df['context'][split_separators[split][0]:split_separators[split][1]].to_list()
-        else:
-            if (dataset_name == "pavlopoulos20"):
-                self.df = self.df[self.df['context'].isna()]  # take only those where context is NaN
+        self.contexts = self.df['context'][split_separators[split][0]:split_separators[split][1]].to_list()
         self.texts = self.df['target'][split_separators[split][0]:split_separators[split][1]].to_list()
         self.labels = self.df['label'][split_separators[split][0]:split_separators[split][1]].to_list()
 
